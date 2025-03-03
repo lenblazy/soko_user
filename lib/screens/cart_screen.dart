27de558f4b29b4af3e7914559b616f216cd41f2b
Lennox Mwabonje <lenblazy@gmail.com>
2025-03-03 15:17:24 +0300

    Add reusable empty screen

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:soko_user/services/asset_manager.dart';
+import 'package:soko_user/widgets/empty_bag.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Cart Screen"),
-      ),
-    );
+    return Scaffold(
+        body: EmptyBagWidget(
+      imagePath: AssetManager.shoppingBasket,
+      title: "Your Cart Is Empty",
+      subtitle: "Looks like your cart is empty, Add some items to get started",
+      buttonText: "Shop Now",
+    ));
   }
 }
---
+++ lib/widgets/empty_bag.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:soko_user/widgets/subtitle_text.dart';
+import 'package:soko_user/widgets/title_text.dart';
+
+import '../services/asset_manager.dart';
+
+class EmptyBagWidget extends StatelessWidget {
+  const EmptyBagWidget({
+    super.key,
+    required this.imagePath,
+    required this.title,
+    required this.subtitle,
+    required this.buttonText,
+  });
+
+  final String imagePath, title, subtitle, buttonText;
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Column(
+      children: [
+        SizedBox(height: 50),
+        Image.asset(
+          imagePath,
+          width: double.infinity,
+          height: size.height * 0.35,
+        ),
+        const SizedBox(height: 20),
+        TitleTextWidget(
+          label: "Whoops",
+          fontSize: 40,
+          color: Colors.red,
+        ),
+        const SizedBox(height: 20),
+        SubtitleTextWidget(
+          label: title,
+        ),
+        const SizedBox(height: 20),
+        SubtitleTextWidget(
+          label: subtitle,
+        ),
+        const SizedBox(height: 20),
+        ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            elevation: 0,
+            backgroundColor: Colors.red,
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+          ),
+          onPressed: () {},
+          child: Text(buttonText),
+        )
+      ],
+    );
+  }
+}
