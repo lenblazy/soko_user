5c7328f6fc064cb9abb626e04382e43ec5e977c7
Lennox Mwabonje <lenblazy@gmail.com>
2025-03-03 12:32:08 +0300

    Add app colors

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 
+import '../consts/app_colors.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.lightScaffoldColor,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
