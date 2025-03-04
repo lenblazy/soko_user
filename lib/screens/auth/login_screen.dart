e0680f6bdba83f13ee8819ca83e3715730e9094f
Lennox Mwabonje <lenblazy@gmail.com>
2025-03-04 13:58:39 +0300

    Add login screen

---
+++ lib/screens/auth/login_screen.dart
@@ -0,0 +1,170 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_iconly/flutter_iconly.dart';
+import 'package:soko_user/consts/validator.dart';
+import 'package:soko_user/widgets/app_name_text.dart';
+import 'package:soko_user/widgets/subtitle_text.dart';
+import 'package:soko_user/widgets/title_text.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  late final TextEditingController emailController;
+  late final TextEditingController passwordController;
+
+  late final FocusNode emailFocusNode;
+  late final FocusNode passwordFocusNode;
+
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    super.initState();
+    emailController = TextEditingController();
+    passwordController = TextEditingController();
+    emailFocusNode = FocusNode();
+    passwordFocusNode = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    if (mounted) {
+      emailController.dispose();
+      passwordController.dispose();
+      emailFocusNode.dispose();
+      passwordFocusNode.dispose();
+    }
+
+    super.dispose();
+  }
+
+  Future<void> _loginFct() async {
+    final isValid = _formKey.currentState!.validate();
+    FocusScope.of(context).unfocus();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        body: Column(
+          children: [
+            const SizedBox(height: 60),
+            const AppNameTextWidget(),
+            const SizedBox(height: 16),
+            Form(
+              key: _formKey,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SingleChildScrollView(
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const SizedBox(height: 10),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: TitleTextWidget(label: "Welcome Back"),
+                        ),
+                        const SizedBox(height: 10),
+                        TextFormField(
+                          controller: emailController,
+                          focusNode: emailFocusNode,
+                          textInputAction: TextInputAction.next,
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: InputDecoration(
+                            hintText: "Email Address",
+                            prefixIcon: Icon(IconlyLight.message),
+                          ),
+                          validator: (value) {
+                            return Validator.emailValidator(value);
+                          },
+                        ),
+                        const SizedBox(height: 10),
+                        TextFormField(
+                          controller: passwordController,
+                          focusNode: passwordFocusNode,
+                          textInputAction: TextInputAction.done,
+                          keyboardType: TextInputType.visiblePassword,
+                          decoration: InputDecoration(
+                            hintText: "Password",
+                            prefixIcon: Icon(IconlyLight.lock),
+                          ),
+                          onFieldSubmitted: (value) async {
+                            await _loginFct();
+                          },
+                          validator: (value) {
+                            return Validator.passwordValidator(value);
+                          },
+                        ),
+                        const SizedBox(height: 16),
+                        Align(
+                          alignment: Alignment.centerRight,
+                          child: TextButton(
+                            onPressed: () {},
+                            child: const SubtitleTextWidget(
+                              label: "Forgot Password?",
+                              textDecoration: TextDecoration.underline,
+                              fontStyle: FontStyle.italic,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 16),
+                        SizedBox(
+                          width: double.infinity,
+                          child: ElevatedButton.icon(
+                            icon: const Icon(Icons.login),
+                            label: Text("Login"),
+                            onPressed: () async {
+                              await _loginFct();
+                            },
+                          ),
+                        ),
+                        const SizedBox(height: 16),
+                        const SubtitleTextWidget(
+                          label: "Or connect using",
+                        ),
+                        const SizedBox(height: 16),
+                        Row(children: [
+                          ElevatedButton(
+                            child: Text("Sign in with google?"),
+                            onPressed: () async {
+                              await _loginFct();
+                            },
+                          ),
+                          ElevatedButton(
+                            child: Text("Guest login?"),
+                            onPressed: () async {
+                              await _loginFct();
+                            },
+                          ),
+                        ]),
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const SubtitleTextWidget(
+                                label: "Don't have an account?",
+                              ),
+                              TextButton(
+                                onPressed: () {},
+                                child: const SubtitleTextWidget(
+                                  label: "Sign up",
+                                  textDecoration: TextDecoration.underline,
+                                  fontStyle: FontStyle.italic,
+                                ),
+                              ),
+                            ]),
+                      ]),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
