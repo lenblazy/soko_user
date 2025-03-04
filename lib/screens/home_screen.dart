7a557751a7666401cdf047e3d4aa3d9c3f083eab
Lennox Mwabonje <lenblazy@gmail.com>
2025-03-04 11:16:43 +0300

    Add categories widget

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:soko_user/consts/app_constants.dart';
 import 'package:soko_user/widgets/app_name_text.dart';
+import 'package:soko_user/widgets/products/ctg_rounded_widget.dart';
 import 'package:soko_user/widgets/products/latest_arrival.dart';
 import 'package:soko_user/widgets/title_text.dart';
 
@@ -27,41 +28,65 @@ class HomeScreen extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(height: 15),
-            SizedBox(
-              height: size.height * 0.25,
-              child: Swiper(
-                itemBuilder: (BuildContext context, int index) {
-                  return Image.asset(
-                    AppConstants.bannerImages[index],
-                    fit: BoxFit.fill,
-                  );
-                },
-                itemCount: AppConstants.bannerImages.length,
-                pagination: const SwiperPagination(
-                  // alignment: Alignment.bottomCenter,
-                  builder: DotSwiperPaginationBuilder(
-                    activeColor: Colors.red,
-                    color: Colors.white,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 15),
+              SizedBox(
+                height: size.height * 0.25,
+                child: Swiper(
+                  itemBuilder: (BuildContext context, int index) {
+                    return Image.asset(
+                      AppConstants.bannerImages[index],
+                      fit: BoxFit.fill,
+                    );
+                  },
+                  itemCount: AppConstants.bannerImages.length,
+                  pagination: const SwiperPagination(
+                    // alignment: Alignment.bottomCenter,
+                    builder: DotSwiperPaginationBuilder(
+                      activeColor: Colors.red,
+                      color: Colors.white,
+                    ),
                   ),
                 ),
               ),
-            ),
-            const SizedBox(height: 15),
-            const TitleTextWidget(label: "Latest Arrival"),
-            const SizedBox(height: 15),
-            SizedBox(
-              height: size.height * 0.2,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: 10,
-                itemBuilder: (context, index) => LatestArrivalProductWidget(),
+              const SizedBox(height: 15),
+              const TitleTextWidget(label: "Latest Arrival"),
+              const SizedBox(height: 15),
+              SizedBox(
+                height: size.height * 0.2,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 10,
+                  itemBuilder: (context, index) => LatestArrivalProductWidget(),
+                ),
               ),
-            ),
-          ],
+              const TitleTextWidget(label: "Categories"),
+              const SizedBox(height: 15),
+              GridView.count(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                crossAxisCount: 4,
+                children: [
+                  const CtgRoundedWidget(
+                      image: AssetManager.mobiles, name: "Phones"),
+                  const CtgRoundedWidget(
+                      image: AssetManager.mobiles, name: "Phones"),
+                  const CtgRoundedWidget(
+                      image: AssetManager.mobiles, name: "Phones"),
+                  const CtgRoundedWidget(
+                      image: AssetManager.mobiles, name: "Phones"),
+                  const CtgRoundedWidget(
+                      image: AssetManager.mobiles, name: "Phones"),
+                  const CtgRoundedWidget(
+                      image: AssetManager.mobiles, name: "Phones"),
+
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
